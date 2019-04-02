577c13b0b4666c62a6f47e67d88f1254fdacfae6
mckhaannn <mt222wz@student.lnu.se>
2019-04-02 14:34:42 +0200

    added homepage to pages folder

---
+++ client/lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        child: Text('hej')
+      ),
+    );
+  }
+}
