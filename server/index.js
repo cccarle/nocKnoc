a250edeb5e83bff5e34b14ea5d67aab2fae211f3
Karl <ke222pf@student.lnu.se>
2019-04-02 10:33:57 +0200

    setting up express and port on 5000

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
-// testing server branch
-// test 2
-// test 3
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+const http = require('http')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.get('/', (req,res) => {
+    res.json({message: "Hello world"})
+})
+
+let server = http.createServer(app).listen(PORT, function () {
+    console.log('Express started on Port' + PORT)
+    console.log('Press Ctrl-C to terminate...')
+  })
