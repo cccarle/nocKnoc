48db8e4568f5f56b05e0eae7c40babe506787e2f
mckhaannn <mt222wz@student.lnu.se>
2019-04-02 14:53:13 +0200

    added appbar with the company logo

--- client/lib/pages/home.dart
+++ client/lib/pages/home.dart
@@ -4,12 +4,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.0), // here the desired height
+        child: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Image.asset(
+                  'assets/Logo.png',
+                  fit: BoxFit.contain,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      body: Container(
-        child: Text('hej')
-      ),
+      body: Container(child: Text('hej')),
     );
   }
 }
