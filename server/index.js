6ef11fe2defd5c1c5b3c8237b75a61b4ab70c098
joel <zorcey@gmail.com>
2019-04-03 14:05:28 +0200

    Now sends form

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
-const http = require("http")
-const bodyParser = require("body-parser")
+const http = require('http')
+const bodyParser = require('body-parser')
 
-let apiRoutes = require("./routes/apiRoutes")
+let apiRoutes = require('./routes/apiRoutes')
 
 app.use(bodyParser.json())
-app.use("/api", apiRoutes)
+app.use('/api', apiRoutes)
 
-http.createServer(app).listen(PORT, function() {
-  console.log("Express started on Port" + PORT)
-  console.log("Press Ctrl-C to terminate...")
+http.createServer(app).listen(PORT, function () {
+  console.log('Express started on Port' + PORT)
+  console.log('Press Ctrl-C to terminate...')
 })
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,25 +1,23 @@
-const server = require("express").Router()
+const server = require('express').Router()
 const employeesController = require('../controllers/employeesController')
 const channelsController = require('../controllers/channelsController')
 const message = require('../utils/slack/api')
 
-  server.get("/employees", async (req, res) => {
+server.get('/employees', async (req, res) => {
     let result = await employeesController.getAll()
     console.log(result)
     res.json(result)
   })
 
-  server.get("/channels", async (req, res) => {
+server.get('/channels', async (req, res) => {
     let result = await channelsController.getAll()
     console.log(result)
     res.json(result)
   })
-  server.get("/helloWorld", async (req, res) => {
-    let result = await message.sendMessageToChannel()
+server.get('/helloWorld', async (req, res) => {
+    let result = await message.sendFormToChannel()
     console.log(result)
     res.json(result)
   })
-
-
 
 module.exports = server
