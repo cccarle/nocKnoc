567bf11ab0bbb5d88ef5a8e4d92f5b48b54e6881
joel <zorcey@gmail.com>
2019-05-14 13:40:15 +0200

    github messes with me

--- server/utils/apiWithCache.js
+++ server/utils/apiWithCache.js
@@ -21,6 +21,7 @@ const getAllUsers = () => {
     userCache.data = getUsersFromApi()
   } else {
     console.log('Using Cache for users')
+    console.log('GITHUB????')
   }
   return userCache.data
 }
